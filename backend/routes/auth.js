dc8f5accf0388f30b14dbd1a6e78978b3ea82e75
AlejandroHernandez1802 <u20181165894@usco.edu.co>
2022-07-15 15:30:14 -0500

    Authorization routes created and also the update user route.

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const CryptoJS = require('crypto-js');
+const jwt =  require('jsonwebtoken');
 
 //Register route
 router.post("/register", async (req,res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password,
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASSWORD_SEC).toString(),
     });
 
     try{
@@ -17,6 +19,35 @@ router.post("/register", async (req,res) => {
         res.status(500).json(err);
     }
     
+});
+
+
+//Login route
+router.post("/login", async (req, res) => {
+    try{
+        const user = await User.findOne({username:req.body.username});
+
+        //Condition to throw error if the user account is not found in the database.
+        !user && res.status(401).json("User not found");
+
+        const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASSWORD_SEC);
+        const stringPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+        
+        //Condition to throw error if the password is not correct
+        stringPassword !== req.body.password && res.status(401).json("Incorrect password");
+
+        //Using JWT to give the user access token
+        const accessToken = jwt.sign({id:user._id, isAdmin: user.isAdmin}, process.env.JWT_SEC, {expiresIn:"3d"});
+
+        //Saving the user info to send as response except the password
+        const { password, ...others } = user._doc;
+
+        //If there's no errros, this message will be thrown
+        res.status(200).json({...others, accessToken});
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
 });
 
 
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,26 @@
 const router = require('express').Router();
+const CryptoJS = require('crypto-js');
+const User = require('../models/User');
+const { verifyTokenAndAuthorization } = require('./verifyToken');
 
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    
+    if(req.body.password) {
+        req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.PASSWORD_SEC).toString();
+    }
 
+    try{
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, {new:true});
+
+        res.status(200).json(updatedUser);
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
+
+});
 
 
 
